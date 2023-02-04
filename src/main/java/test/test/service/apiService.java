3bafafacd43ab019db643bab7bb18212ee639fe1
CHANWOO LEE <chw210601@gmail.com>
2023-02-05 00:13:22 +0900

    no message

---
+++ src/main/java/test/test/service/apiService.java
@@ -0,0 +1,44 @@
+package test.test.service;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+
+public class ApiService {
+    public String getData(String url ) {
+        //Spring restTemplate
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        String jsonInString = "";
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders header = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(header);
+
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+
+        ResponseEntity<?> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Object.class);
+
+        result.put("statusCode", resultMap.getStatusCodeValue()); //http status code를 확인
+        result.put("header", resultMap.getHeaders()); //헤더 정보 확인
+        result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
+
+        //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
+//        ObjectMapper mapper = new ObjectMapper();
+//        jsonInString = mapper.writeValueAsString(resultMap.getBody());
+
+
+        return jsonInString;
+
+
+    }
+}
---
+++ src/main/java/test/test/service/kakaomapService.java
@@ -0,0 +1,10 @@
+package test.test.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class kakaomapService {
+
+
+
+}
